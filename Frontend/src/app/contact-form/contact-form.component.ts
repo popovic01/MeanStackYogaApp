6eecb47971f7301ab5013acea3e83a3a4bcd20d5
popovic01 <mimica2001@gmail.com>
2021-08-08 00:49:25 +0200

    contact

--- Frontend/src/app/contact-form/contact-form.component.ts
+++ Frontend/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,6 +11,13 @@ export class ContactFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  send(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(form.value.fullName, form.value.email, form.value.subject, form.value.message);
   }
 
 }
